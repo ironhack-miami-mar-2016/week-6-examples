41bd435dd895e6745ad5862cc27b5019d92dd204
Leo Correa <lcorr005@gmail.com>
2016-05-02 12:16:59 -0400

    Add create action

--- Sandwichr/app/controllers/sandwiches_controller.rb
+++ Sandwichr/app/controllers/sandwiches_controller.rb
@@ -5,4 +5,15 @@ class SandwichesController < ApplicationController
     render json: sandwiches
   end
 
+  def create
+    sandwich = Sandwich.create(sandwich_params)
+    render json: sandwich
+  end
+
+  private
+
+  def sandwich_params
+    params.require(:sandwich)
+      .permit(:name, :bread_type)
+  end
 end
