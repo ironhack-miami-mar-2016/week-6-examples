cbd9b2a11dd683bbf1a4af0a2643d066fcd6959a
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-05-03 17:39:55 -0400

    Devise: Add permit name fields in registration

--- devise_example/app/controllers/application_controller.rb
+++ devise_example/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :permit_extra_devise_fields, if: :devise_controller?
+
+
+  protected
+    def permit_extra_devise_fields
+      permit_array = devise_parameter_sanitizer.for(:sign_up)
+      permit_array.push( :first_name, :last_name )
+    end
 end
